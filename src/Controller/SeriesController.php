401b0252284f31ddde25542c3065712786ff7cad
luizvictor <luizvictor.dev@outlook.com>
2023-06-28 22:53:30 -0300

    feat: creating controller

---
+++ src/Controller/SeriesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SeriesController extends AbstractController
+{
+    #[Route('/series', name: 'app_series')]
+    public function index(): Response
+    {
+        $seriesList = [
+            'Sopranos',
+            'The Wire',
+            'Breaking bad'
+        ];
+
+        return $this->render('series/index.html.twig', [
+            'controller_name' => 'SeriesController',
+        ]);
+    }
+}
