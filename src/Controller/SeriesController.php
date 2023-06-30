4babab0ae3cce8e5800ccc32a017d50f41319c3e
luizvictor <luizvictor.dev@outlook.com>
2023-06-29 21:51:20 -0300

    feat: creating entity

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -2,20 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Series;
+use App\Repository\SeriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SeriesController extends AbstractController
 {
+    private SeriesRepository $seriesRepository;
+
+    public function __construct(SeriesRepository $seriesRepository)
+    {
+        $this->seriesRepository = $seriesRepository;
+    }
+
     #[Route('/series', name: 'app_series', methods: ['GET'])]
     public function index(): Response
     {
-        $seriesList = [
-            'Sopranos',
-            'The Wire',
-            'Breaking bad'
-        ];
+        $seriesList = $this->seriesRepository->findAll();
 
         return $this->render('series/index.html.twig', [
             'seriesList' => $seriesList,
@@ -26,5 +33,15 @@ class SeriesController extends AbstractController
     public function create(): Response
     {
         return $this->render('series/create.html.twig');
+    }
+
+    #[Route('/series/create', methods: ['POST'])]
+    public function store(Request $request): Response
+    {
+        $name = $request->request->get('name');
+        $serie = new Series($name);
+
+        $this->seriesRepository->save($serie, true);
+        return new RedirectResponse('/series');
     }
 }
